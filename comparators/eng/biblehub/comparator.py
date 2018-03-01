9e7e5f89e26e7805712c132839e7d1bc40ed7e4f
Daniel Lewis <daniellewis777@gmail.com>
2018-03-01 23:26:21 +0000

    Still working on KJV

--- comparators/eng/biblehub/comparator.py
+++ comparators/eng/biblehub/comparator.py
@@ -1,12 +1,14 @@
 import os
+import re
 import xml.etree.ElementTree
-from py.loaders import load_checks
+from py.loaders import load_checks, load_psalms_check
 
 
 class BibleHubComparator:
 
     def __init__(self, version):
 
+        self.psalms = load_psalms_check(version)
         checks = load_checks(version)
         self.prefer = checks.get('comparator').get('prefer')
 
@@ -58,11 +60,7 @@ class BibleHubComparator:
                     comparator_verse = comparator_verse[1:]
 
                 else:
-
                     print('\n')
-                    print(f'{self.version} - {ref}')
-                    print(f'{self.version} - my verse:')
-
                     print(f'{self.version} - {ref}')
                     print(f'{self.version} - my verse:')
                     print(f'{self.version} - {omv}')
@@ -75,9 +73,59 @@ class BibleHubComparator:
         # print(f'preferred_verse = {preferred_verse}')
         return preferred_verse
 
+    def compare_psalm(self, my_verse, comparator_verse, ref, psalm):
+
+        omv = my_verse
+        ocv = comparator_verse
+
+        verse_zero = psalm[0]
+        verse_one = psalm[1]
+
+        for prefer in self.prefer:
+            # print(my_verse.startswith(prefer[0]))
+            # print(comparator_verse.startswith(prefer[1]))
+
+            allow = True
+            if len(prefer) > 3:
+                allow = ref == prefer[3]
+
+            if allow:
+                my_verse = my_verse.replace(prefer[0], prefer[2])
+                comparator_verse = comparator_verse.replace(prefer[1], prefer[2])
+
+        print(verse_one in my_verse)
+        print(verse_one in comparator_verse)
+        print(verse_zero in my_verse)
+        print(verse_zero in comparator_verse)
+
+        if verse_one in my_verse and verse_one in comparator_verse and (verse_zero in my_verse or verse_zero in comparator_verse):
+            # TODO - Return both verses
+            return verse_one
+        else:
+            print('\n')
+            print(f'{self.version} - {ref}')
+            print(f'{self.version} - my verse:')
+            print(f'{self.version} - {omv}')
+            print(f'{self.version} - comparator verse:')
+            print(f'{self.version} - {ocv}')
+            print(f'{self.version} - verse zero:')
+            print(f'{self.version} - {verse_zero}')
+            print(f'{self.version} - verse one:')
+            print(f'{self.version} - {verse_one}')
+            raise Exception(f'{self.version} - BibleHubComparator psalm error')
+
     def compare(self, verse):
         my_verse = verse
         comparator_verse = self.rows[self.row].findall('Cell')[self.column].find('Data').text
         ref = self.rows[self.row].findall('Cell')[0].find('Data').text
         self.row += 1
-        return my_verse if my_verse == comparator_verse else self.preferred(my_verse, comparator_verse, ref)
+
+        chapter = re.findall('(\d+):', ref)[0]
+        psalm = self.psalms.get(chapter)
+
+        if ref.startswith('Ps') and ref.endswith(':1') and psalm is not None:
+            return self.compare_psalm(my_verse, comparator_verse, ref, psalm)
+        elif my_verse == comparator_verse:
+            return my_verse
+        else:
+            return self.preferred(my_verse, comparator_verse, ref)
--- py/loaders.py
+++ py/loaders.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 
 def load_checks(version):
@@ -10,3 +11,16 @@ def load_checks(version):
         checks = my_json.get(version, {})
 
     return checks
+
+
+def load_psalms_check(version):
+    paths = Path('psalms').glob(f'**/{version}.json')
+    paths = list(paths)
+    path = paths[0] if len(paths) else None
+    psalms = None
+
+    if path is not None:
+        with open(path) as data:
+            psalms = json.load(data)
+
+    return psalms
